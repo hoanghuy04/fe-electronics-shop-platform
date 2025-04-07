c314d9679e316cea283c9572b0c44a61e187c55d
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2025-04-08 00:06:32 +0700

    config StepCart component

--- src/components/StepCart.jsx
+++ src/components/StepCart.jsx
@@ -1,4 +1,4 @@
-import { Steps } from "antd";
+import { Steps, ConfigProvider } from "antd";
 import { IdCard, ShoppingBag, CreditCard, ShieldCheck } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
@@ -25,30 +25,38 @@ export default function StepCart(props) {
   ];
 
   const onChange = (value) => {
-    if (value === 0) {
-      navigate("/cart");
-    } else if (value === 1) {
-      navigate("/cart/step-two");
-    } else if (value === 2) {
-      navigate("/cart/step-three");
-    }
+    if (value === 0) navigate("/cart");
+    else if (value === 1) navigate("/cart/step-two");
+    else if (value === 2) navigate("/cart/step-three");
   };
 
   const stepsDisabled = (index) => index > current;
 
   return (
     <div className="bg-white p-3">
-      <div className="py-3 px-5 rounded-sm bg-blue-50">
-        <Steps current={current} onChange={onChange} labelPlacement="vertical">
-          {items.map((item, index) => (
-            <Steps.Step
-              key={index}
-              title={item.title}
-              icon={item.icon}
-              disabled={stepsDisabled(index)}
-            />
-          ))}
-        </Steps>
+      <div className="py-3 px-5 rounded-sm bg-red-50">
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#ff4d4f",
+            },
+          }}
+        >
+          <Steps
+            current={current}
+            onChange={onChange}
+            labelPlacement="vertical"
+          >
+            {items.map((item, index) => (
+              <Steps.Step
+                key={index}
+                title={item.title}
+                icon={item.icon}
+                disabled={stepsDisabled(index)}
+              />
+            ))}
+          </Steps>
+        </ConfigProvider>
       </div>
     </div>
   );
